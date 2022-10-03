4905db346b972a4db4c3acb06f6a17f94db61a52
mattt <97069726+NotADevGuy@users.noreply.github.com>
2022-10-02 21:11:05 -0400

    Perfectionism : (

--- HW2/Email/EmailClient.py
+++ HW2/Email/EmailClient.py
@@ -10,10 +10,8 @@ class Client:
         ptfx_port = 25
         dvct_port = 110
 
-        print()
-
         while True:
-            choice = input("Welcome to email service\nCommands: SEND, RECEIVE, IP (change IP), QUIT\n>> ")
+            choice = input("\nWelcome to email service\nCommands: SEND, RECEIVE, IP (change IP), QUIT\n>> ")
 
             if choice == "SEND":
                 try:
@@ -51,20 +49,62 @@ class Client:
                     print("Something happened... is the IP right?")
 
             elif choice == "RECEIVE":
-                print("Get the email")
+                try:
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.connect((host, dvct_port))
+                        s.recv(1024).decode()
+
+                        user = input(">> Username: ")
+                        s.sendall(f"user {user}\n".encode())
+                        s.recv(1024).decode()
+
+                        pswd = input(">> Password (very secure, you can trust us): ")
+                        s.sendall(f"pass {pswd}\n".encode())
+
+                        code = s.recv(1024).decode()
+                        if code[:4] == "-ERR":
+                            print("Incorrect password or username! Try Again.")
+                        else:
+                            s.sendall("LIST\n".encode())
+                            count = ((s.recv(1024).decode()).split(" "))[1]
+                            print()
+                            print(f"Login successful, {count} messages!")
+                            print(f"Commands: LIST, READ, or QUIT")
+                            while True:
+                                choice = input(">> ")
+                                choice = choice.split(" ")
+                                if choice[0] == "LIST" and len(choice) == 1:
+                                    s.sendall("LIST\n".encode())
+                                    print(s.recv(1024).decode())
+                                elif choice[0] == "READ":
+                                    if len(choice) == 1:
+                                        num = input(">> Email #: ")
+                                        s.sendall(f"RETR {num}\n".encode())
+                                        print(s.recv(1024).decode())
+                                    elif len(choice) == 2:
+                                        print(f"RETR {choice[1]}")
+                                        s.sendall(f"RETR {choice[1]}\n".encode())
+                                        print(s.recv(1024).decode())
+                                    else:
+                                        print("Incorrect format. Try Again.\n")
+                                elif choice[0] == "QUIT":
+                                    choice = ''
+                                    break
+                                else:
+                                    print("Unrecognized command or command format. Try Again.\n")
+                except:
+                    print("Something happened... is the IP right?")
 
             elif choice == "IP":
                 host = input(">> Enter email server IP: ")
                 print(f"IP Changed to {host}")
 
             elif choice == "QUIT":
-                print("Have a good day!")
+                print("Have a good day! Thanks for using my email service!")
                 break
 
             else:
                 print("Command not recognized.")
-
-            print('')
 
 
 if __name__ == '__main__':
